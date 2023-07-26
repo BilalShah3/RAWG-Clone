33f360e4a82b66a92a8d17992a46cc1aeb91189c
BilalShah3 <2460@students.riphah.edu.pk>
2023-07-26 21:15:59 +0500

    Display genres & fixed layout issue

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,19 @@
+import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react'
 import useGenres from '../hooks/useGenres'
+import getCroppedImageUrl from '../services/image-url'
 const GenreList = () => {
   const {data:genres} = useGenres()
   return (
-    <ul>
-      {genres.map(genre => <li key={genre.id}>{genre.name}</li>)}
-   </ul>
+    <List>
+      {genres.map(genre => 
+        <ListItem key={genre.id} paddingY="5px">
+           <HStack>
+              <Image src={getCroppedImageUrl(genre.image_background)} borderRadius={8} alt={genre.name} boxSize="32px" />
+              <Text fontSize='lg'>{genre.name}</Text>
+           </HStack>
+        </ListItem>
+      )}
+   </List>
          
   )
 }
