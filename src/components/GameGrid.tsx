3085581f72acc78ce31fd6ebfbd4b8082476af10
BilalShah3 <2460@students.riphah.edu.pk>
2023-07-29 23:36:06 +0500

    feat: implement infinite queries (Load more data)

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,32 +1,43 @@
-import { SimpleGrid, Text } from "@chakra-ui/react"
+import { Box, Button, SimpleGrid, Text } from "@chakra-ui/react"
 import useGames from "../hooks/useGames"
 import GameCard from "./GameCard"
 import GameCardSkeleton from "./GameCardSkeleton"
 import GameCardContainer from "./GameCardContainer"
 import { GameQuery } from "../App"
+import React from "react"
 
 interface GameGridProps {
   gameQuery: GameQuery
 }
 
 const GameGrid = ({gameQuery}: GameGridProps) => {
-  const{data:games, error, isLoading}= useGames(gameQuery)
+  const{data:games, error, isLoading, fetchNextPage, isFetchingNextPage, hasNextPage}= useGames(gameQuery)
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8]
   if (error) return <Text>{error.message}</Text>
   return (
-      
-      <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} padding="10px" spacing={5}>
+     <Box padding="10px">
+      <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} spacing={5}>
         {isLoading && skeletons.map((skeleton) => (
           <GameCardContainer key={skeleton}>
             <GameCardSkeleton />
           </GameCardContainer>
         ))}
-        {games?.results.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
-          </GameCardContainer>
-        ))}
+         {games?.pages.map((page, index) => (
+            <React.Fragment key={index}>
+                {page.results.map((game) => (
+                  <GameCardContainer key={game.id}>
+                    <GameCard game={game} />
+                  </GameCardContainer>
+                ))}
+            </React.Fragment>
+          ))}
       </SimpleGrid>
+      {hasNextPage && (
+        <Button marginY={5} onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
+          {isFetchingNextPage ? "Loading..." : "Load More"}
+        </Button>
+      )}
+    </Box>
   )
 }
 
