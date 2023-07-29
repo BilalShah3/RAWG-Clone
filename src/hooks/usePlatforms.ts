eb4f7a7eecf32c306b49d2a05719fc222905c73f
BilalShah3 <2460@students.riphah.edu.pk>
2023-07-30 00:27:28 +0500

    fix: add next property in initial data

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query"
 import platforms from "../data/platforms"
-import APIClient from "../services/api-client"
+import APIClient, { FetchResponse } from "../services/api-client"
 
 const apiClient = new APIClient<Platform>('/platforms/lists/parents')
 
@@ -11,10 +11,9 @@ export interface Platform {
 }
 
 const usePlatforms = () => useQuery({
-  
   queryKey: ['platforms'],
   queryFn: apiClient.getAll,
   staleTime: 24 * 60 * 60 * 1000, // 1 day
-  initialData: {count: platforms.length, results: platforms},
+  initialData: {count: platforms.length, next: null, results: platforms},
 })
 export default usePlatforms
