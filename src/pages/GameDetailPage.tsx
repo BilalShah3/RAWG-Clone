eca4e38bc644168521611b6f3b7b819e4c9621a3
BilalShah3 <2460@students.riphah.edu.pk>
2023-08-02 00:47:42 +0500

    Build game screenshots component

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -4,6 +4,7 @@ import { Center, Heading, Spinner } from "@chakra-ui/react"
 import ExpandableText from "../components/ExpandableText"
 import GameAttributes from "../components/GameAttributes"
 import GameTrailer from "../components/GameTrailer"
+import GameScreenshots from "../components/GameScreenShots"
 
 const GameDetailPage = () => {
   const { slug } = useParams()
@@ -23,6 +24,7 @@ const GameDetailPage = () => {
       <ExpandableText limit={300}>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
       <GameTrailer gameId={game.id} />
+      <GameScreenshots gameId={game.id} />
     </>
   )
 }
